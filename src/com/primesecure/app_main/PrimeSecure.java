181775237171fcd7e1feaf20fd9a0c13f5dc9891
Jennifer <Jennifer@Jennifer_Galaxy>
2025-07-02 23:13:01 -0400

    Agrego clases PrimeSecure y PrimesList

---
+++ src/com/primesecure/app_main/PrimeSecure.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.primesecure.app_main;
+
+import com.primesecure.servicios.PrimesList;
+
+/**
+ * Sistema de gestión de números primos para PrimeSecure
+ * 
+ * @author Jennifer y Guiselle
+ */
+
+public class PrimeSecure {
+
+    public static void main(String[] args) {
+        PrimesList primesList = new PrimesList();
+        System.out.println(primesList.isPrime(5));
+    }
+
+}
---
+++ src/com/primesecure/servicios/PrimesList.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.primesecure.servicios;
+
+import java.util.ArrayList;
+
+/**
+ * Clase para representar una lista de números primos
+ * 
+ * @author Jennifer y Guiselle
+ */
+
+public class PrimesList extends ArrayList<Integer> {
+
+    // Método isPrime para verificar si un número es primo
+    public boolean isPrime(int numero) {
+        if (numero <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Método getPrimesCount que devuelve la cantidad de números primos en la lista
+    public int getPrimesCount() {
+        return this.size();
+    }
+
+    // Sobreescritura del método add()
+    @Override
+    public boolean add(Integer numero) {
+        if (isPrime(numero)) {
+            return super.add(numero);
+        } else {
+            throw new IllegalArgumentException("Número inválido: El número no es primo.");
+        }
+    }
+
+    // Sobreescritura del método remove()
+    @Override
+    public boolean remove(Object o) {
+        if (o instanceof Integer && isPrime((Integer) o)) {
+            return super.remove(o);
+        } else {
+            throw new IllegalArgumentException("Eliminación inválida: Solo se pueden eliminar números primos.");
+        }
+    }
+
+}
