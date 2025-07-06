418aa38a44cea22ea2f27f0d5cb334e68b55143e
Guiselle Sepulveda <guiselle.sepulveda89@gmail.com>
2025-07-06 14:57:00 -0400

    menu principal

--- src/com/primesecure/servicios/PrimesList.java
+++ src/com/primesecure/servicios/PrimesList.java
@@ -20,8 +20,15 @@ public class PrimesList extends ArrayList<Integer> {
             return false;
         }
 
-        for (int i = 2; i <= Math.sqrt(numero); i++) {
-            if (numero % i == 0) {
+        if (numero <= 3) {
+            return true;
+        }
+
+        if (numero % 2 == 0 || numero % 3 == 0) {
+            return false;
+        }
+        for (int i = 5; i * i <= numero; i += 6) {
+            if (numero % i == 0 || numero % (i + 2) == 0) {
                 return false;
             }
         }
